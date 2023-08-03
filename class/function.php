5550211cd3fc989c28820ed0e3b4c3b57b72f2fc
Jonathon Roges <jhonny.roges@gmail.com>
2023-08-03 16:19:35 -0300

    Criação das telas usuario, login, cadastro de usuario e tela profile

--- class/function.php
+++ class/function.php
@@ -1,33 +1,159 @@
 <?php
 
+$mysqli = conectar();//Inicia uma conexão com o banco de dados
+
+function conectar(){//conecta ao BASE de dados e passa o objeto conexão
+  
+   //local host
+   $servidor = 'localhost';
+   $usuario = 'root';
+   $senha = '';//sem senha - senha padrão do PhpMyAdmin
+   $banco = 'meet';
+ 
+	
+	// Conecta-se ao banco de dados MySQL
+	
+	$mysqli = new mysqli($servidor, $usuario, $senha, $banco);
+	//gravando com ascentuação
+   mysqli_query($mysqli,"SET NAMES 'utf8'");
+   mysqli_query($mysqli,'SET character_set_connection=utf8');
+   mysqli_query($mysqli,'SET character_set_client=utf8');
+   mysqli_query($mysqli,'SET character_set_results=utf8');
+	// Caso algo tenha dado errado, exibe uma mensagem de erro
+	 if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
+   
+	  return $mysqli;//retorna um objeto conexão
+   }
+
+
    function CadUser(){
    ?>
-   <script type="text/javascript" src="../js/jquery-1.11.1.js"></script>
-   <script type="text/javascript" src="../js/ajaxupload.3.5.js" ></script>
-   <script type="text/javascript" src="../js/myScripts.js"></script>
-
+   
    <!-- upload files-->
 
+  <div class="box-cad-user">
+	  <div class="container-user">
    <form action="main.php" method="POST">
-			<div class='files'></div>
-			<div id="upload" >Carregar Foto</div>
+	   
+	   
+			<BR>CADASTRO<BR>
+		   <div class='files'></div>
+			<div id="upload" class="carregar-foto" >Carregar Foto</div>
 			<span id="status" ></span>
-			Nome:<br>
-			<input type="text" id="userName" name="userName"><br>
-			Data Nascimento:<br>
-			<input type="text" id="userBirthday" name="usereBirthday"><br>
-			Telefone:<br>
-			<input type="text" id="userPhone" name="userPhone"><br>
-			Bio:<br>
-			<textarea placeholder="Digite algo sobre você" id="userBio" name="userBio"
-              rows="5" cols="33" >
-            </textarea><br>
-			<input type="submit" value="alterar" name="enviar" style="background-color:#066; color:#FFF; padding:10px; border:none;">
+			<br>
+			<input type="text" id="userName" name="userName" placeholder="Nome" ><br>
+			<br>
+			<input type="password" id="userPassword" name="userPassword" placeholder="senha" ><br>
+			<br>
+			<input type="text" id="userBirthday" name="usereBirthday" value="__/__/____" placeholder="data nascimento"><br>
+	        <br> 
+			<select name="userSexo" id="userSexo">
+		      <option value="male">masculino</option>
+		      <option value="female" selected>feminino</option>
+		    </select><br>
+			<br>
+			<input type="text" id="userPhone" name="userPhone" placeholder="telefone"><br>
+	        <br>
+			<input type="text" id="userEmail" name="userEmail" placeholder="email"><br>
+			<br>
+			<textarea  id="userBio" name="userBio"
+              rows="5" cols="33"  maxlength ="150">Digite algo sobre você
+            </textarea><br><br>
+			<input type="submit" value="gravar" name="opc" id="bt-enviar-cad-user" >
    </form>
+	  </div><!--container-user -->
+ </div>	<!--box-cad-user -->
+
+<!-- Ações javaScript-->
+ 
+ <script type="text/javascript">
+	    $("#userBirthday").mask("99/99/9999");
+		$("#userPhone").mask("(99) 99999-9999");
+</script>
+
+
    <?php
    }
 
-   CadUser();//chama user();
+function windowLoginUser(){
+	echo "<div class='box-info-center'>
+	           <span> 
+	              ";
+                   ?>
+                    <form action="main.php" method="POST">
+						<span>EFETUAR LOGIN</span><br><br>
+					   <input type="text" id="userEmail" name="userEmail" placeholder="email"><br><br>
+					   <input type="password" id="userPassword" name="userPassword" placeholder="senha" ><br>
+					   <input type="submit" value="logar" name="opc" id="bt-login-user" >
+                    </form>
+
+				   <?php
+           echo "			  
+			   </span>
+			</div>";
+}
+
+function areaUser(){
+
+	 $mysqli = conectar();
+	
+	$sql = "SELECT * FROM user 
+	                 WHERE user_email = '".trim($_POST["userEmail"])."'   
+	                 AND  user_password = '".trim($_POST["userPassword"])."'  ";
+	// 
+				  
+	$query = $mysqli->query($sql);
+	$numRows =  $query->num_rows;//número de linhas
+	//print "->".$_POST["userEmail"]." - ".$_POST["userPassword"]."numRows = ".$numRows ."";
+	while (    $dados = $query->fetch_assoc()  ) {
+				  //print "usuario encontrado '".$dados["user_name"]."";
+				  if (!isset($_SESSION))
+				  session_start(); 
+				  $_SESSION['user_id'] = $dados["user_id"] ;
+                   ?>
+                  <div class='box-geral-profile'>
+                        
+				        <div class="box-info-tex-profile">
+							<br><br>
+						    <span class="text-info-name"> <?php print $dados["user_name"];  ?></span><br>
+							<span class="text-info-bio">  <?php print $dados["user_bio"];  ?></span>	
+						</div>
+
+						<div class="box-moldura-profile"
+								style="background-image: url('../images/users/media_<?php print $dados["user_photo_perfil"] ;?>'" >
+								<div class="moldura-round-profile"></div>
+						</div>
+	             </div>	
+				 
+				 <div class="box-action-profile">
+					<div class="box-bt"><span>Editar perfil</span></div>
+					<div class="box-bt"><span>publicar</span></div>
+	             </div>
+                  
+
+
+				  <?php
+                  
+
+
+
+	 }
+
+     //CONTRUINDO LEYOU DA TELA DE LOGIN DO USUÁRIO
+	 if($numRows >= 1){//SE ENCONTRAR ALGUMA LINHA
+        
+		//print "Usuario ID: ".$_SESSION['user_id'];
+
+	 }else{
+		print "USUÁRIO OU SENHA NÃO ENCONTRADOS";
+	 }
+
+
+
+}
+
+
+   
 
 
 
@@ -96,6 +222,15 @@
 			
 			}
 	}
+
+	function setFormatAmericanDate($date){
+		//yyyy-mm-dd 
+		$pieces = explode("/", $date);
+		
+		return $pieces[2].'-'.$pieces[1].'-'.$pieces[0];
+		
+	}
+
 
 
 ?>
--- class/main.php
+++ class/main.php
@@ -1,5 +1,78 @@
 <?php
 //session_start();//inicializo a sessao
   include "function.php";//Para recortar a imagem
-  
-  ?>
+  $mysqli = conectar();//Inicia uma conexão com o banco de dados
+?>
+ <!--Função principal-->
+ <html>
+	 <head>
+		  <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		    <link href="../styles/style.css" rel="stylesheet">
+		    <script type="text/javascript" src="../js/jquery-1.11.1.js"></script>
+			<script type="text/javascript" src="../js/ajaxupload.3.5.js" ></script>
+			<script type="text/javascript" src="../js/myScripts.js"></script>
+			<script type="text/javascript" src="../js/cdn.es.gov.br_scripts_jquery_jquery-maskedinput_1.4.1_jquery.maskedinput-1.4.1.min.js"></script>
+
+		  <title>meet</title>
+	 </head>
+	 <body>
+		 <div class="main">
+			 <?php
+			 
+			    if(isset($_POST["opc"])){
+					
+
+							if($_POST["opc"] == "gravar"){
+								
+									$sql = "INSERT INTO user (       
+									user_name    ,
+									user_password,
+									user_birthday,
+									user_sexo    ,
+									user_phone   ,
+									user_email   ,
+									user_bio     ,
+									user_photo_perfil)
+									values(
+									'".$_POST['userName']."'      ,
+									'".$_POST['userPassword']."'  ,
+									'".setFormatAmericanDate(   trim($_POST['usereBirthday'] )  )."' ,
+									'".$_POST['userSexo']."'      ,
+									'".$_POST['userPhone']."'     ,
+									'".$_POST['userEmail']."'     ,
+									'".$_POST['userBio']."'       ,
+									'".$_POST['arquivo']."')"; 
+									
+
+									$query = $mysqli->query($sql);
+									print "<div class='box-info-center'>
+												<span> 
+												Dados Gravados com sucesso!
+												</span>
+										</div>	 
+											
+											";
+							}
+							if($_POST["opc"] == "logar"){
+							
+								areaUser();
+							}
+					
+
+				}else{
+					
+					//CadUser();//chama user();
+                    windowLoginUser();
+
+				}
+			    
+			 ?>
+		 </div>
+	 </body>
+ </html>
+
+<?php
+
+
+?>
